14fda83f4c839f00519c7fe625193be7a84e2962
TomasCall <vas.bence01@gmail.com>
2022-09-18 19:33:06 +0200

    #try
    try

--- 1.Házi/sztring_konkret.py
+++ 1.Házi/sztring_konkret.py
@@ -2,6 +2,7 @@ def main():
     user_input = input("Kérlek írd le a nevedet: ")
     user_input = user_input.strip()
     print("Hello "+ user_input +"!")
+    #try
 
 if __name__=="__main__":
     main()
